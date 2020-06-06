49f9537f6a707a0fc40819a0bfb12b118f228f1c
Ji Zhi Qian <jizhi.qian@jimubox.com>
2020-06-06 17:05:10 +0800

    Mock skip

--- src/test/java/com/hogwarts/testcase/TestExec.java
+++ src/test/java/com/hogwarts/testcase/TestExec.java
@@ -30,7 +30,7 @@ public class TestExec {
         city = JSONParaser.getJsonValue(weatherinfo, "city");
         Reporter.log("用例结果: resultCode=>expected: " + exp_city + " ,actual: " + city);
         Assert.assertEquals(city, exp_city);
-//        throw new SkipException("");
+        throw new SkipException("");
     }
 
     @Test(groups = {"BaseCase"}, description = "验证北京天气返回城市名称是否正确")
